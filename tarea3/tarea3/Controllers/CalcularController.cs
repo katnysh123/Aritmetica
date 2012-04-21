82f917444f11b2eadca9acb946ef3a574941aa6f
unknown <Owner@Owner-HP.(none)>
2012-04-21 07:56:13 -0400

    tarea de Aritmetica

---
+++ tarea3/tarea3/Controllers/CalcularController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using tarea3.Models;
+
+namespace tarea3.Controllers
+{
+    public class CalcularController : Controller
+    {
+        //
+        // GET: /Calcular/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Calcular datos)
+        {
+            ViewBag.r = datos.a + datos.b;
+            return this.View();
+        }
+
+
+        public ActionResult Sumar(Calcular datos)
+        {   ViewBag.r = datos.a + datos.b;
+            return this.View();
+        }
+          
+        public ActionResult Restar(Calcular datos) {
+            ViewBag.r = datos.a - datos.b;
+            return this.View();
+        }
+          public ActionResult Multiplicar(Calcular datos)
+          {   ViewBag.r = datos.a * datos.b;
+              return this.View();
+          }
+          
+          public ActionResult Dividir(Calcular datos)
+          {   ViewBag.r = datos.c / datos.d;
+              return this.View();
+          }
+
+         
+    }
+}
---
+++ tarea3/tarea3/Models/Calcular.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace tarea3.Models
+{
+    public class Calcular
+    {
+        public int a { set; get; }
+        public int b { set; get; }
+        public float c { set; get; }
+        public float d { set; get; }
+        
+    }
+}
